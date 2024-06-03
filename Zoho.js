a9c2d0832fbf85dcbd58306d99506e5c143f285e
Sander <sander.v@softtouch.be>
2024-06-03 17:31:24 +0200

    localstorage toegevoegd

--- Zoho.js
+++ Zoho.js
@@ -13,46 +13,75 @@ var Taak_ = document.getElementById("Taak_");
 var Mail_ = document.getElementById("Mail_");
 var Banner_ = document.getElementById("Banner_");
 var url = document.getElementById("url");
+let instelling;
+
+class storageItem {
+    constructor(begroeting1, begroeting2, profo, naam, taak, mail, banner_foto, banner_url) {
+        this.begroeting1 = begroeting1;
+        this.begroeting2 = begroeting2;
+        this.profo = profo;
+        this.naam = naam;
+        this.taak = taak;
+        this.mail = mail;
+        this.banner_foto = banner_foto;
+        this.banner_link = banner_url;
+    }
+}
 
 window.addEventListener("load", init)
 
 function init(){
-    //console.log(begroeting1.innerText, begroeting2.innerText, profo.firstChild.src, naam.innerText, Banner.src, Banner.parentElement.parentElement.href)
-    console.log(Taak, Mail.firstChild.href.split(":")[1], Mail.firstChild.innerText)
-    fline.value = begroeting1.innerText
-    sline.value = begroeting2.innerText
-    profo_.value = profo.firstChild.src
-    naam_.value = naam.innerText
-    Taak_.value = Taak.innerText
-    Mail_.value = Mail.firstChild.innerText
-    Banner_.value = Banner.src
-    url.value = Banner.parentElement.parentElement.href
+    let persoonlijkeInstelling = localStorage.getItem("persoonlijkeInstelling");
+    instelling = new storageItem(begroeting1.innerText, begroeting2.innerText, profo.firstChild.src, naam.innerText, Taak.innerText, Mail.firstChild.innerText, Banner.src, Banner.parentElement.parentElement.href);
+    if(persoonlijkeInstelling == null){
+        localStorage.setItem("persoonlijkeInstelling", JSON.stringify(instelling))
+    } else {
+        instelling = JSON.parse(localStorage.getItem("persoonlijkeInstelling"));
+    }
+    console.log(persoonlijkeInstelling, "test", instelling)
+
+    //Tekst aanpassen inputvelden
+    fline.value = instelling.begroeting1
+    sline.value = instelling.begroeting2
+    profo_.value = instelling.profo
+    naam_.value = instelling.naam
+    Taak_.value = instelling.taak
+    Mail_.value = instelling.mail
+    Banner_.value = instelling.banner_foto
+    url.value = instelling.banner_link
+
+    //Tekst aanpassen van copy-tekst
+    var Mailto = Mail.firstChild.href.split(":")[0] + ":" + Mail_.value
+    begroeting1.innerText = instelling.begroeting1
+    begroeting2.innerText = instelling.begroeting2
+    profo.firstChild.src = instelling.profo
+    naam.innerText = instelling.naam
+    Taak.innerText = instelling.taak
+    Mail.firstChild.innerText = instelling.mail
+    Mail.firstChild.href = Mailto
+    Banner.src = instelling.banner_foto
+    Banner.parentElement.parentElement.href = instelling.banner_link
 }
 
 function copy(){
     var to_copy = document.getElementById("kopy").outerHTML;
-    console.log(to_copy)
     navigator.clipboard.writeText(to_copy);
     alert("Gekopy't");
 }
 
 function exchange(){
     var Mailto = Mail.firstChild.href.split(":")[0] + ":" + Mail_.value
-    console.log(Mailto)
-    begroeting1.innerText = fline.value
-    begroeting2.innerText = sline.value
-    profo.firstChild.src = profo_.value.split("&amp;").join("&");
-    naam.innerText = naam_.value
-    Taak.innerText = Taak_.value
-    Mail.firstChild.innerText = Mail_.value
+    instelling = new storageItem(fline.value, sline.value, profo_.value.split("&amp;").join("&"), naam_.value, Taak_.value, Mail_.value, Banner_.value.split("&amp;").join("&"), url.value)
+    localStorage.setItem("persoonlijkeInstelling", JSON.stringify(instelling))
+    begroeting1.innerText = instelling.begroeting1
+    begroeting2.innerText = instelling.begroeting2
+    profo.firstChild.src = instelling.profo
+    naam.innerText = instelling.naam
+    Taak.innerText = instelling.taak
+    Mail.firstChild.innerText = instelling.mail
     Mail.firstChild.href = Mailto
-    Banner.src = Banner_.value.split("&amp;").join("&")
-    Banner.parentElement.parentElement.href = url.value
-
-    /*console.log(profo.firstChild.src.split("&"), profo_.value.split("&amp;"))
-    console.log(profo.firstChild.src.split("&")[1], profo.firstChild.src.split("&")[2])
-    console.log(profo_.value.split("&amp;")[1], profo_.value.split("&amp;")[2])*/
+    Banner.src = instelling.banner_foto
+    Banner.parentElement.parentElement.href = instelling.banner_link
 }
 
-// https://desk.zoho.com:443/support/ImageDisplay?downloadType=uploadedFile&amp;fileName=1717406591750.jpg&amp;blockId=edbsn0005cf141d1b394a62bb09407a40ac4f6cca6e7043ffb83a3f268f0ce9f83ec6&amp;zgId=edbsncae68e23a9524156b4f6211ddb91faf8&amp;mode=view
 
